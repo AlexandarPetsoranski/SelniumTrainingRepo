03ded027b1fc77e9acf1fca865f53fe55b3e9ada
AlexandarPetsoranski <alexandarpetsoranski@coherentsolutions.com>
2021-12-01 14:53:37 +0200

    downloadTest tests added

--- src/test/java/spec/seleniumSpec/ProgressBarSpec.java
+++ src/test/java/spec/seleniumSpec/ProgressBarSpec.java
@@ -1,10 +1,8 @@
 package spec.seleniumSpec;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.seleniumTraining.StylishProgressBarPage;
 import spec.BaseSpec;
@@ -12,30 +10,20 @@ import spec.BaseSpec;
 import java.time.Duration;
 
 public class ProgressBarSpec extends BaseSpec {
-
-    private StylishProgressBarPage stylishProgressBarPage;
+    StylishProgressBarPage stylishProgressBarPage;
 
     @Test
-    public void verifyObjectNotDownloadedIfPageReloaded() {
+    public void downloadTest() {
         stylishProgressBarPage = new StylishProgressBarPage(driver);
         WebElement downloadButton = driver.findElement(stylishProgressBarPage.getDOWNLOAD_BUTTON());
         downloadButton.click();
+        new WebDriverWait(driver,Duration.ofSeconds(20)).until(driver -> {
+            String currentPercentText = driver.findElement(By.cssSelector(".percenttext")).getText();
+            int currentPercent = Integer.parseInt(currentPercentText.split("%")[0]);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
-        WebElement inputPercent = driver.findElement(stylishProgressBarPage.getPERCENT_INPUT());
-
-        wait.until((ExpectedCondition<Boolean>) driver -> {
-            boolean hasClass = downloadButton.getAttribute("class").contains("active");
-
-            while (hasClass) {
-                int hiddenInputPercent = Integer.parseInt(inputPercent.getAttribute("value"));
-                if (hiddenInputPercent >= 50) {
-                    break;
-                }
-            }
-            return true;
+            return currentPercent >= 50;
         });
+
         driver.navigate().refresh();
     }
 }
